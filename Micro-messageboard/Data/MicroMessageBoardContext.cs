89ac6ed972c87a2fed51f3df75c3038d28d275c5
Ramirez <ramirez.alberto@zoho.com>
2022-09-01 21:50:53 -0700

    Create the DbContext and add the service

---
+++ Micro-messageboard/Data/MicroMessageBoardContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Micro_messageboard.Models;
+
+namespace Micro_messageboard.Data
+{
+    public class MicroMessageBoardContext : DbContext
+    {
+        public MicroMessageBoardContext(DbContextOptions<MicroMessageBoardContext> options) : base(options)
+            { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
--- Micro-messageboard/Program.cs
+++ Micro-messageboard/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Micro_messageboard.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MicroMessageBoardContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("MicroMessageBoardContext") ?? throw new InvalidOperationException("Connection string 'MvcArticleContext' not found.")));
 
 var app = builder.Build();
 

ad1973f52eba0780cfc542fc046f772f4bd350fd
Ramirez <ramirez.alberto@zoho.com>
2022-09-01 20:11:12 -0700

    Agregar archivos de proyecto.

---
+++ Micro-messageboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Micro_messageboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

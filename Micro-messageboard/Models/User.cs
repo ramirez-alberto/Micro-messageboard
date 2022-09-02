28df84f9f5e4467fff5df8498c270ddedf754179
Ramirez <ramirez.alberto@zoho.com>
2022-09-01 21:50:21 -0700

    Create the User model

---
+++ Micro-messageboard/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Micro_messageboard.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(12, MinimumLength = 4)]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(15, MinimumLength = 8)]
+        public string Password { get; set; }
+
+        public DateTime CreationTime { get; set; }
+        
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+}

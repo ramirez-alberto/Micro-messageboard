6d8285aeb0b070ded9182b3426c73131bad611c5
Alberto <ramirez-alberto@zoho.com>
2022-09-02 00:06:28 -0700

    Assign a value to creationtime and updatetime

--- Micro-messageboard/Data/MicroMessageBoardContext.cs
+++ Micro-messageboard/Data/MicroMessageBoardContext.cs
@@ -6,8 +6,35 @@ namespace Micro_messageboard.Data
     public class MicroMessageBoardContext : DbContext
     {
         public MicroMessageBoardContext(DbContextOptions<MicroMessageBoardContext> options) : base(options)
-            { }
+        { }
 
-        public DbSet<User> Users { get; set; }
+        public DbSet<User> Users { get; set; } = default!;
+
+                /*specifying singular table names*/
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is User && (
+                        e.State == EntityState.Added
+                        || e.State == EntityState.Modified));
+
+            foreach (var entityEntry in entries)
+            {
+                ((User)entityEntry.Entity).UpdateTime = DateTime.Now;
+
+                if (entityEntry.State == EntityState.Added)
+                {
+                    ((User)entityEntry.Entity).CreationTime = DateTime.Now;
+                }
+            }
+
+            return base.SaveChangesAsync();
+        }
     }
 }
